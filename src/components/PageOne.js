9cae2a54929d10569262932660172da9a043be8d
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-01 02:59:56 -0700

    add page components to switch through

---
+++ src/components/PageOne.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageOne() {
+  return (
+    <h2>Have you gone through all the steps on the Learn How to Program debugging lesson?</h2>
+  )
+}
---
+++ src/components/PageThree.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageThree() {
+  return (
+    <h2>Have you spent 15 minutes going through the problem documenting every step?</h2>
+  )
+}
