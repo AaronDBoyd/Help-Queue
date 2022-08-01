cb2a74ad7ff8dde0afac533130b2dbb968531efa
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-01 02:55:46 -0700

    import TicketControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 // import './App.css';
 import React from "react";
 import Header from "./Header"
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 
 function App() {
   
   return (
     <React.Fragment>
       <Header />
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
