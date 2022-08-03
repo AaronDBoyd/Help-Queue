f9823addfb93b362676872c45a48d5dd4dc78a29
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-03 03:11:44 -0700

    create form to add ticket

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -11,7 +11,8 @@ class TicketControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false, 
-      count: 0
+      count: 0,
+      mainTicketList: []
     };
   }
 
@@ -25,67 +26,75 @@ class TicketControl extends React.Component {
     }) 
   }
 
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({mainTicketList: newMainTicketList,
+      formVisibleOnPage: false,
+    /* count: 0  */});
+    }
+
   handleClick = () => {
     this.setState(prevState => ({ formVisibleOnPage: !prevState.formVisibleOnPage
     }));
   }
 
-  // render(){
-  //   let currentlyVisibleState = null;
-  //   let buttonText = null; 
-  //   if (this.state.formVisibleOnPage) {
-  //     currentlyVisibleState = <NewTicketForm />
-  //     // currentlyVisibleState = <PageOne />
-  //     buttonText = "Return to Ticket List";
-  //   } else {
-  //     currentlyVisibleState = <TicketList />
-  //     buttonText = "Add Ticket";
-  //   }
-  //   return (
-  //     <React.Fragment>
-  //       {currentlyVisibleState}
-  //       <button onClick={this.handleClick}>{buttonText}</button>
-  //     </React.Fragment>
-  //   )
-  // }
-
-
+  
   render(){
     let currentlyVisibleState = null;
     let buttonText = null; 
-    switch ( this.state.count ) {
-      case 0:
-        currentlyVisibleState = <TicketList />
-        buttonText = "Add Ticket";
-        break;
-      case 1:
-        currentlyVisibleState = <PageOne />
-        buttonText = "Okay then, Add Ticket";
-        break;
-      case 2:
-        currentlyVisibleState = <PageTwo />
-        buttonText = "Yes";
-        break;
-      case 3:
-        currentlyVisibleState = <PageThree />
-        buttonText = "I did";
-        break;
-      case 4:
-        currentlyVisibleState = <NewTicketForm />
-        buttonText = "Return to Ticket List";
-        break;
-        default:
-          currentlyVisibleState = <TicketList />
-          buttonText = "Add Ticket";
-          break;  
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+      // currentlyVisibleState = <PageOne />
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />
+      buttonText = "Add Ticket";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={this.updateState}>{buttonText}</button>
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     )
   }
+
+
+  // render(){
+  //   let currentlyVisibleState = null;
+  //   let buttonText = null; 
+  //   switch ( this.state.count ) {
+  //     case 0:
+  //       currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />
+  //       buttonText = "Add Ticket";
+  //       break;
+  //     case 1:
+  //       currentlyVisibleState = <PageOne />
+  //       buttonText = "Okay then, Add Ticket";
+  //       break;
+  //     case 2:
+  //       currentlyVisibleState = <PageTwo />
+  //       buttonText = "Yes";
+  //       break;
+  //     case 3:
+  //       currentlyVisibleState = <PageThree />
+  //       buttonText = "I did";
+  //       break;
+  //     case 4:
+  //       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+  //       buttonText = "Return to Ticket List";
+  //       break;
+  //       default:
+  //         currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />
+  //         buttonText = "Add Ticket";
+  //         break;  
+  //   }
+  //   return (
+  //     <React.Fragment>
+  //       {currentlyVisibleState}
+  //       <button onClick={this.updateState}>{buttonText}</button>
+  //     </React.Fragment>
+  //   )
+  // }
 }
 
 export default TicketControl;
