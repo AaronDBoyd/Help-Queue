35dff8af17e8c7c96c284b79193a181ec4fe0300
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-10 19:38:39 -0700

    write third test

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,17 +1,18 @@
 const reducer = (state = {}, action) => {
   const { names, location, issue, id } = action;
   switch (action.type) {
-    case 'ADD_TICKET':
+    case "ADD_TICKET":
       return Object.assign({}, state, {
         [id]: {
           names: names,
-          location: location, 
-          issue: issue, 
-          id: id
-        }
+          location: location,
+          issue: issue,
+          id: id,
+        },
       });
-      default:
-        return state;
+    
+    default:
+      return state;
   }
 };
 
