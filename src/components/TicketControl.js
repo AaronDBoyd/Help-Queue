bc6ca06f037acf9bb4058e2a00dc415e37785f30
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-14 15:05:54 -0700

    update functions to use action helpers

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -5,6 +5,7 @@ import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm'
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
+import * as a from './../actions';
 // import PageOne from "./PageOne";
 // import PageTwo from "./PageTwo";
 // import PageThree from "./PageThree";
@@ -32,19 +33,11 @@ class TicketControl extends React.Component {
 
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = newTicket;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id, 
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    
+    const action = a.addTicket(newTicket);
     dispatch(action);
 
-    const action2 = {
-      type: 'TOGGLE_FORM' 
-    }
+    const action2 = a.toggleForm();
     dispatch(action2);
   };
 
@@ -62,19 +55,14 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
     }
   };
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: 'DELETE_TICKET',
-      id: id
-    }
+    const action = a.deleteTicket(id);
     dispatch(action);
 
     this.setState({selectedTicket: null});
@@ -87,14 +75,7 @@ class TicketControl extends React.Component {
 
   handleEditingTicketInList = (ticketToEdit) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(ticketToEdit);
     dispatch(action);
 
     this.setState({
