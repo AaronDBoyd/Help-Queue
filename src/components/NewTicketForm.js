a5b9af5a58ad7b796a07b5380b437b7521e9f9fc
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-21 23:33:13 -0700

    addTicketToFirestore function

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -8,25 +8,42 @@ import { useFirestore } from 'react-redux-firebase'
 
 export default function NewTicketForm(props) {
 
-  function handleNewTicketFormSubmission(event) {
+  // function handleNewTicketFormSubmission(event) {
+  //   event.preventDefault();
+  //   props.onNewTicketCreation({
+  //     names: event.target.names.value, 
+  //     location: event.target.location.value, 
+  //     issue: event.target.issue.value, 
+  //     id: v4(),
+  //     timeOpen: new Date(),
+  //     formattedWaitTime: formatDistanceToNow(new Date(), {
+  //       addSuffix: true
+  //     })
+  //   });
+  // }
+
+  const firestore = useFirestore();
+
+  function addTicketToFirestore(event) {
     event.preventDefault();
-    props.onNewTicketCreation({
-      names: event.target.names.value, 
-      location: event.target.location.value, 
-      issue: event.target.issue.value, 
-      id: v4(),
-      timeOpen: new Date(),
-      formattedWaitTime: formatDistanceToNow(new Date(), {
-        addSuffix: true
-      })
-    });
+
+    return firestore.collection('tickets').add(
+      {
+        names: event.target.names.value,
+        location: event.target.location.value,
+        issue: event.target.issue.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    )
   }
+
 
   return (
     <React.Fragment>
       <h1>- Submit a Ticket -</h1>
       <ReusableForm 
-        formSubmissionHandler={handleNewTicketFormSubmission}
+        // formSubmissionHandler={handleNewTicketFormSubmission}
+        formSubmissionHandler={addTicketToFirestore}
         buttonText="Help!" />
     </React.Fragment>
   );
